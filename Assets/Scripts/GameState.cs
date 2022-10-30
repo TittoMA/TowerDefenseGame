7ec1491bc3c579ddc00426263047f3e0bb3e8524
TittoMA <tittomahogany@gmail.com>
2022-10-30 19:42:29 +0700

    add timer

--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameState : MonoBehaviour
 {
     public static bool gameIsPaused = false;
+    public float timeRemaining = 60.0f;
     public GameObject gameOver;
+    public GameObject gameWin;
+    public Text timeText;
 
     // Start is called before the first frame update
     void Start()
@@ -17,7 +21,18 @@ public class GameState : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(!gameIsPaused){
+
+            if(timeRemaining > 0){ 
+                timeRemaining -= Time.deltaTime;
+                timeText.text = timeRemaining.ToString("0.00");
+            }
+        }
+
+        if(timeRemaining <= 0) {
+            Pause();
+            gameWin.SetActive(true);
+        }
     }
 
     public void Pause()
